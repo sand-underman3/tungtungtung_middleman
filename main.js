c0f33a201dddfa5fa865ae1db42d31f44fed5603
Sand <saand.underman3@gmail.com>
2026-05-20 21:06:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // lol
+import express from 'express';
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+router.get('/', (req, res) => {
+  res.json({ message: 'GET request' });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({ message: 'POST request', data: body });
+});
+
+app.use('/', router);
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
