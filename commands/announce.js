6e5a1035211663b0e474b2efcb0e8159eab4b022
Sand <saand.underman3@gmail.com>
2026-05-21 22:46:50 -0500

    I WILL FUCKING KILL THIS ONE SINGULAR PERIOD

--- commands/announce.js
+++ commands/announce.js
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 const { default: embeds } = await import('../cmdModules/embeds.js');
 
-import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
