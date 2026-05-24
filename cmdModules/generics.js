f4c15eabfef6eb6caa4aea237b237cec7171aa2e
Sand <saand.underman3@gmail.com>
2026-05-24 00:36:07 -0500

    test

---
+++ cmdModules/generics.js
@@ -0,0 +1,5 @@
+export default {
+    testNumeric(str) {
+        return /^\d+$/.test(str)
+    }
+}
--- commands/ownership_update.js
+++ commands/ownership_update.js
@@ -1,4 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
+import generics from '../cmdModules/generics.js';
+const { testNumeric } = generics;
+
 const { default: embeds } = await import('../cmdModules/embeds.js');
 
 import { prisma } from '../prisma.js';
@@ -7,8 +10,8 @@ const sand = '584493443032547373'
 
 export default {
     definition: new SlashCommandBuilder()
-      .setName('whitelist')
-      .setDescription('[R] Whitelist the use of this bot.')
+      .setName('game add')
+      .setDescription('[R] Whitelist the use of this bot on a certain game.')
       .setIntegrationTypes([0, 1]) 
       .setContexts([0, 1, 2])
       
@@ -16,7 +19,13 @@ export default {
         option.setName('user_id')
         .setRequired(true)
         .setDescription("The discord user ID of whom to provide access.")
+      )
+      .addStringOption(option=>
+        option.setName('game_id')
+        .setRequired(true)
+        .setDescription('The game ID.')
       ),
+
     handler: async (interaction) => {
         const authorId = interaction.user.id
 
@@ -26,10 +35,24 @@ export default {
         }
 
         const userId = interaction.options.getString('user_id')
+        const gameId = interaction.options.getString('game_id')
+
+        if (!testNumeric(gameId)) {
+          await interaction.editReply({content: "The game ID is not a number."});
+          return;
+        }
+        
+        const placeResponse = await fetch(`https://apis.roblox.com/universes/v1/places/${gameId}/universe`);
+        const placeData = await placeResponse.json();
+        const universeId = placeData.universeId;
+
+        const response = await fetch(`https://games.roblox.com/v1/games?universeIds=${universeId}`);
+        const data = await response.json();
+        const gameName = data.data[0].name;
 
         const [titleMsg, description, fields] = [
             'Whitelist added',
-            `<@${userId}> has been added to ${interaction.client.user.username}'s whitelist.`,
+            `<@${userId}> has been added to "${gameName}"'s whitelist.`,
             []
         ]
         
